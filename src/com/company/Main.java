67094613653fdddab018727d062cae71288e1915
Highlevelyuri <kriucov61944zce3.com@gmail.com>
2018-02-03 09:44:11 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int x = 1;
+	do {
+	    x++;
+        System.out.println(Math.sin(x));
+    }
+    while (x<20);
+    }
+}
